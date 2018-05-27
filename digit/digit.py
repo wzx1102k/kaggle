58ad4e6821efe84cc2a91398f30bd832a50fdb42
wzx1102k <409747794@qq.com>
2018-05-27 08:02:57 -0700

    Update
    Update

--- digit/digit.py
+++ digit/digit.py
@@ -14,9 +14,9 @@ training_df = pd.read_csv(training_path)
 testing_df = pd.read_csv(testing_path)
 
 
-training_set = np.array(training_df.iloc[:, 1:].astype('float32')) / 255
-training_labels = np.array(training_df.iloc[:, :1].astype('int'))
-test_set = np.array(testing_df. iloc[:, :].astype('float32')) / 255
+training_set = np.array(training_df.iloc[1:4000, 1:].astype('float32')) / 255
+training_labels = np.array(training_df.iloc[1:4000, :1].astype('int'))
+test_set = np.array(testing_df. iloc[1:10, :].astype('float32')) / 255
 print(training_set.shape)
 print(training_labels.shape)
 
@@ -24,10 +24,12 @@ image = training_set
 
 labels = np.zeros((image.shape[0], DIGIT_RANGE))
 
+#print(training_set)
 
 for i in range(0, image.shape[0]):
     labels[i, training_labels[i]] = 1
 
+#print(labels)
 
 def weight_variable(name, shape):
     return(tf.get_variable(name, shape=shape, initializer=tf.contrib.layers.xavier_initializer()))
@@ -54,25 +56,25 @@ with g.as_default():
     pool_heigth = IMAGE_HEIGHT
     pool_width = IMAGE_WIDTH
     with tf.device('/gpu:0'):
-        W_conv1 = weight_variable('W1', [5, 5, 1, 16])
-        b_conv1 = bias_variable('b1', [16])
+        W_conv1 = weight_variable('W1', [5, 5, 1, 32])
+        b_conv1 = bias_variable('b1', [32])
         h_conv1 = tf.nn.relu(tf.nn.bias_add(con2d(x_image, W_conv1), b_conv1))
         h_pool1, pool_heigth, pool_width = max_pooling_2x2(h_conv1, pool_heigth, pool_width)  # output 14 14
         h_pool1 = tf.nn.dropout(h_pool1, keep_prob)
 
-        W_conv2 = weight_variable('W2', [5, 5, 16, 32])
-        b_conv2 = bias_variable('b2', [32])
+        W_conv2 = weight_variable('W2', [5, 5, 32, 64])
+        b_conv2 = bias_variable('b2', [64])
         h_conv2 = tf.nn.relu(tf.nn.bias_add(con2d(h_pool1, W_conv2), b_conv2))
         h_pool2, pool_heigth, pool_width = max_pooling_2x2(h_conv2, pool_heigth, pool_width)  # output 7 7
         h_pool2 = tf.nn.dropout(h_pool2, keep_prob)
-        h_pool2_flat = tf.reshape(h_pool2, [-1, pool_heigth * pool_width * 32])
+        h_pool2_flat = tf.reshape(h_pool2, [-1, pool_heigth * pool_width * 64])
         
-        W_fc3 = weight_variable('W4', [pool_heigth * pool_width * 32, 512])
-        b_fc3 = bias_variable('b4', [512])
+        W_fc3 = weight_variable('W4', [pool_heigth * pool_width * 64, 1024])
+        b_fc3 = bias_variable('b4', [1024])
         h_fc3 = tf.nn.relu(tf.add(tf.matmul(h_pool2_flat, W_fc3), b_fc3))
         h_fc3 = tf.nn.dropout(h_fc3, keep_prob)
         
-        W_fc4 = weight_variable('W5', [512, DIGIT_RANGE])
+        W_fc4 = weight_variable('W5', [1024, DIGIT_RANGE])
         b_fc4 = bias_variable('b5', [DIGIT_RANGE])
         output = tf.add(tf.matmul(h_fc3, W_fc4), b_fc4)
         prediction = tf.nn.softmax(output)
@@ -93,8 +95,9 @@ with g.as_default():
             threads = tf.train.start_queue_runners(coord=coord)
             sess.run(init)
             tf.train.start_queue_runners(sess=sess)
-            for i in range(5000):
-                cross_sess = sess.run(cross, feed_dict={xs: image, ys: labels, keep_prob: 0.5})
+            for i in range(100):
+                train_step.run(feed_dict={xs: image, ys: labels, keep_prob: 0.5})
+                cross_sess = sess.run(cross, feed_dict={xs: image, ys: labels, keep_prob: 1})
                 print(cross_sess)
         
             test_predict = sess.run(output, feed_dict={xs: test_set, keep_prob: 1})
